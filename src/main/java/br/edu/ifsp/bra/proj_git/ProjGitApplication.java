18f6dd9c4a4bf6b179d0b8b142055d69da5c6b14
Professor <gustavo_veras@ifsp.edu.br>
2024-09-05 14:37:13 -0300

    Criação do Projeto

---
+++ src/main/java/br/edu/ifsp/bra/proj_git/ProjGitApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.proj_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/bra/proj_git/ProjGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.bra.proj_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
